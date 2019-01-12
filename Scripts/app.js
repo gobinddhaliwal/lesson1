3b6877a7a4ad8fa9890934ba288dbc1c7de0e783
gdhali41 <300975953@cencol.ca>
2019-01-12 12:08:28 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,11 @@
+let myGlobalVariable =0;
+(function(){
+
+    function Start(){
+        let startvariable=0;
+        console.log(`%cApp Started...${startvariable}`,"font-size: 20px; color:blue;");
+    }
+        window.addEventListener("load",Start);
+    
+
+})();
